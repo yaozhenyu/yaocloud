4f4dccc7a6e13bf220587170d2e9167e91840cc5
yaozhenyu <yaozhenyu2015@outlook.com>
2019-02-24 09:21:56 +0800

    初始化

---
+++ readme.md
@@ -0,0 +1,7 @@
+初始化一个maven项目
+```
+mvn -B archetype:generate \
+  -DarchetypeGroupId=org.apache.maven.archetypes \
+  -DgroupId=com.yaozhenyu \
+  -DartifactId=yaocloud
+```
---
+++ yaocloud/src/main/java/com/yaozhenyu/App.java
@@ -0,0 +1,13 @@
+package com.yaozhenyu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
